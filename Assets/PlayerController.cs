ee3d1ea6d9b2be2fc447112977872708fe71eb68
안단태 <48adt@naver.com>
2019-07-27 17:14:11 +0900

    many things are implemented in PlayerController.cs

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -18,16 +18,50 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float birdWalkingSpeed, birdFlyingSpeed, birdFallingSpeed;
     [SerializeField]
-    private float cowWalkingSpeed, cowFallingSpeed, cowDashDistance;
+    private float cowWalkingSpeed, cowFallingSpeed, cowDashDistance, cowDashTime;
+    [SerializeField]
+    private float birdHeight, cowHeight;
+    [SerializeField]
+    private float birdMaxHealth, cowMaxHealth, maxHealth;   // maxHealth: 현재 모드에서의 최대 체력
     [SerializeField]
     private GameObject hummingBird, blackCow;
 
     private float lastHorizontal = 0f;
+    private float currentDashTime = -1f;    // -1f이면 대시 중이 아님, 0f 이상이면 대시 중, IsDashing으로 확인 가능
+    private float currentDashDirection = 0f;
+    private bool isDead = false;
 
     [HideInInspector]
     public bool isGrounded;
-    [HideInInspector]
-    public bool isDashing;
+
+    public bool IsDashing
+    {
+        // 읽기 전용 프로퍼티
+        get
+        {
+            return currentDashTime >= 0f;
+        }
+    }
+
+    /// <summary>
+    /// 게으름 게이지입니다.
+    /// 새일 때 안 움직이면 증가하고, 100이 되면 소로 변신합니다.
+    /// 소일 때 움직이면 감소하고, 0이 되면 새로 변신합니다.
+    /// </summary>
+    public float LazyWork
+    {
+        get
+        {
+            if (state == State.HummingBird)
+            {
+                return 100f * currentTransformLazy / transformLazy;
+            }
+            else
+            {
+                return 100f * (transformWork - currentTransformWork) / transformWork;
+            }
+        }
+    }
 
     Animator animator;
 
@@ -39,17 +73,17 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        if (isDead) return;
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         bool isWorking; // 일하는 중인지, 즉 키보드 입력을 받고 있거나 대시 중인지
 
         #region 변신
-        if (!(state == State.BlackCow && isDashing) && Mathf.Approximately(moveHorizontal, 0f) && (state == State.BlackCow || Mathf.Approximately(moveVertical, 0f)))
+        if (!(state == State.BlackCow && IsDashing) && Mathf.Approximately(moveHorizontal, 0f) && (state == State.BlackCow || Mathf.Approximately(moveVertical, 0f)))
         {
             isWorking = false;
             if (state == State.HummingBird) currentTransformLazy += Time.fixedDeltaTime;
-            Debug.Log("curLazy = " + currentTransformLazy);
         }
         else
         {
@@ -58,10 +92,10 @@ public class PlayerController : MonoBehaviour
             if (state == State.BlackCow)
             {
                 currentTransformWork += Time.fixedDeltaTime;
-                if (isDashing) currentTransformWork += Time.fixedDeltaTime;
-                Debug.Log("curWork = " + currentTransformWork);
+                if (IsDashing) currentTransformWork += Time.fixedDeltaTime;
             }
         }
+        Debug.Log("LazyWork = " + LazyWork);
 
         if (state == State.HummingBird && currentTransformLazy > transformLazy)
         {
@@ -71,8 +105,10 @@ public class PlayerController : MonoBehaviour
             state = State.BlackCow;
             hummingBird.SetActive(false);
             blackCow.SetActive(true);
+            maxHealth = cowMaxHealth;
+            health = Mathf.Clamp(health * (cowMaxHealth / birdMaxHealth), 0f, maxHealth);   // 체력 비례
         }
-        if (state == State.BlackCow && currentTransformWork > transformWork)
+        if (state == State.BlackCow && currentTransformWork > transformWork && !IsDashing)
         {
             // 새로 변신
             currentTransformWork = 0f;
@@ -80,27 +116,59 @@ public class PlayerController : MonoBehaviour
             state = State.HummingBird;
             blackCow.SetActive(false);
             hummingBird.SetActive(true);
+            maxHealth = birdMaxHealth;
+            health = Mathf.Clamp(health, 0f, maxHealth);
         }
         #endregion
 
-        #region 움직임
+        #region 움직임(새일 경우 체력 감소 포함)
         Vector2 movement = new Vector2(0f, 0f);
         if ( state == State.HummingBird )
         {
+            // 새의 움직임
+            health -= Time.fixedDeltaTime;
             movement.x = moveHorizontal * birdWalkingSpeed;
             movement.y = moveVertical * birdFlyingSpeed -  birdFallingSpeed;
-        } else
+        }
+        else
         {
-            movement.x = moveHorizontal * cowWalkingSpeed;
-            movement.y = isGrounded ? 0f : -cowFallingSpeed;
+            // 소의 움직임
+            if (IsDashing)
+            {
+                // 대시 중
+                currentDashTime += Time.fixedDeltaTime;
+                movement.x = currentDashDirection * (2 * cowDashDistance / cowDashTime) * (1 - currentDashTime / cowDashTime);
+                movement.y = isGrounded ? 0f : -cowFallingSpeed;
+            }
+            else
+            {
+                // 대시 중이 아님
+                movement.x = moveHorizontal * cowWalkingSpeed;
+                movement.y = isGrounded ? 0f : -cowFallingSpeed;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z) && !IsDashing && isGrounded &&
+                Mathf.Abs(moveHorizontal) - Mathf.Abs(lastHorizontal) >= 0f && !Mathf.Approximately(moveHorizontal, 0f))
+            {
+                // 움직이던 방향으로 대시 발동
+                currentDashTime = 0f;
+                currentDashDirection = Mathf.Sign(moveHorizontal);
+            }
+            if (currentDashTime > cowDashTime)
+            {
+                // 대시 끝내기
+                // 벽에 충돌 시 즉시 끝나게 해야 함
+                currentDashTime = -1f;
+                currentDashDirection = 0f;
+            }
         }
         GetComponent<Rigidbody2D>().velocity = movement;
-        // TODO 소일 때 대시 기능
         #endregion
 
         #region 바닥에 닿았는지 감지
         Vector2 ray = new Vector2(0f, -1f);
-        RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(0f, 0.1f, 0f), ray, 0.5f);        //0.02만 올라가도 안 닿음
+        float height = (state == State.HummingBird) ? birdHeight : cowHeight;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position - new Vector3(0f, 0.1f, 0f), ray, height);        // 0.01만 올라가도 안 닿음
         if (hit)
         {
             isGrounded = true;
@@ -135,11 +203,19 @@ public class PlayerController : MonoBehaviour
         else
         {
             animator = blackCow.GetComponent<Animator>();
-            animator.SetBool("IsDashing", isDashing);
+            animator.SetBool("IsDashing", IsDashing);
             animator.SetBool("IsWalk", isWorking && (Mathf.Abs(moveHorizontal) - Mathf.Abs(lastHorizontal) >= 0f));
         }
         #endregion
 
-        lastHorizontal = moveHorizontal;
+        lastHorizontal = moveHorizontal;    // 깔끔한 애니메이션 재생과 대시를 위해 필요
+
+        #region 죽음
+        if (health <= 0f)
+        {
+            isDead = true;
+            // TODO 죽는 애니메이션
+        }
+        #endregion
     }
 }
