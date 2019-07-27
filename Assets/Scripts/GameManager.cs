387162972f5506d42f50f0b22f123ebadd5a1655
안단태 <48adt@naver.com>
2019-07-27 17:36:17 +0900

    create GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (gm != null)
+        {
+            Destroy(this);
+            return;
+        }
+        gm = this;
+        DontDestroyOnLoad(this);
+    }
+    
+}
