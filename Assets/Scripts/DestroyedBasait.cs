d8dd0bc9f4bc25d1b761cb3fc060d6eac364ea08
안단태 <48adt@naver.com>
2019-07-28 01:48:13 +0900

    DestroyedBasalt animation

---
+++ Assets/Scripts/DestroyedBasait.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyedBasait : MonoBehaviour
+{
+    private float lifetime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifetime += Time.deltaTime;
+        if (lifetime > 1f) Destroy(this.gameObject);
+    }
+}
