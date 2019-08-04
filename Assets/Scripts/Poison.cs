b6fd65f85bbf335dc85ce24d4bfe0aa194f03057
안단태 <48adt@naver.com>
2019-08-04 18:53:01 +0900

    poison odd size available

--- Assets/Scripts/Poison.cs
+++ Assets/Scripts/Poison.cs
@@ -8,6 +8,8 @@ public class Poison : MonoBehaviour
     private int widthEven;  // 너비, 반드시 짝수여야 함
     [SerializeField]
     private int height;
+    [SerializeField]
+    private float reduceEdgeSize;
 
     void Awake()
     {
@@ -15,12 +17,12 @@ public class Poison : MonoBehaviour
         ParticleSystem.EmissionModule em = ps.emission;
         em.rateOverTime = new ParticleSystem.MinMaxCurve(widthEven * 10);
         ParticleSystem.ShapeModule sm = ps.shape;
-        sm.radius = widthEven / 2;
+        sm.radius = widthEven / 2f;
         ParticleSystem.MainModule mm = ps.main;
         mm.startSpeed = height;
         BoxCollider2D c = GetComponent<BoxCollider2D>();
         c.offset = new Vector2(0f, height / 2f);
-        c.size = new Vector2(widthEven, height);
+        c.size = new Vector2(widthEven - reduceEdgeSize, height - reduceEdgeSize);
     }
 
     void OnTriggerStay2D(Collider2D other)
